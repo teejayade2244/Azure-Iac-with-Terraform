70506ac289b7a90c1fed852a5bf92869261c0f12
TemitopeAde22 <temitope224468@gmail.com>
2025-06-29 19:07:34 +0100

    replace bastion host with azure managed bastion service

--- web-linux-vmss-output.tf
+++ web-linux-vmss-output.tf
@@ -10,11 +10,3 @@ output "web_linux_vmss_instance_count" {
   value = azurerm_linux_virtual_machine_scale_set.web_vmss.instances
 }
 
-output "web_linux_vmss_primary_network_interface_id" {
-  value = azurerm_linux_virtual_machine_scale_set.web_vmss.network_interface[0].id
-}
-
-output "web_linux_vmss_private_ip_addresses" {
-  description = "Private IP addresses of the VMSS instances"
-  value       = azurerm_linux_virtual_machine_scale_set.web_vmss.network_interface[0].ip_configuration[0].private_ip_address
-}
