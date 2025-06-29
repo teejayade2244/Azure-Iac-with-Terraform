ecc45a2d1fc442e292c4740860f104b125362a20
TemitopeAde22 <temitope224468@gmail.com>
2025-06-29 11:08:33 +0100

    first commit

--- web-lb-output.tf
+++ web-lb-output.tf
@@ -1,26 +1,26 @@
 output "web_lb_public_ip" {
   description = "value of the public IP address for the web load balancer"
-  value = azurerm_public_ip.web_LB_pip.ip_address
+  value = azurerm_public_ip.web_lb_pip.ip_address
 }
 
 output "web_lb_frontend_ip_configuration" {
   description = "value of the frontend IP configuration for the web load balancer"
-  value = azurerm_lb.web_LB.frontend_ip_configuration[0].private_ip_address
+  value = azurerm_lb.web_lb.frontend_ip_configuration[0].private_ip_address
 }
 
 output "web_lb_backend_address_pool_id" {
   description = "ID of the backend address pool for the web load balancer"
-  value = azurerm_lb_backend_address_pool.web_LB_backend_pool.id
+  value = azurerm_lb_backend_address_pool.web_lb_backend_pool.id
 }
 
 output "web_lb_probe_id" {
   description = "ID of the probe for the web load balancer"
-  value = azurerm_lb_probe.web_LB_probe.id
+  value = azurerm_lb_probe.web_lb_probe.id
 }
 
 output "web_lb_rule_id" {
   description = "ID of the rule for the web load balancer"
-  value = azurerm_lb_rule.web_LB_rule.id
+  value = azurerm_lb_rule.web_lb_rule.id
 }
 
 output "web_nic_backend_pool_association_id" {
