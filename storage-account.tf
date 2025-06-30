17e24629c7851ca9d5d446b65f46daf60a6edee5
TemitopeAde22 <temitope224468@gmail.com>
2025-06-30 01:42:42 +0100

    replace bastion host with azure managed bastion service

--- storage-account.tf
+++ storage-account.tf
@@ -1,15 +1,15 @@
-resource "azurerm_storage_account" "storage_account" {
-  name                     = "${local.resource_name_prefix}-storageaccount"
-  resource_group_name = azurerm_resource_group.my_resource_group.name
-  location            = azurerm_resource_group.my_resource_group.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  tags = local.common_tags
-}
+# resource "azurerm_storage_account" "storage_account" {
+#   name                     = "${local.resource_name_prefix}-storageaccount"
+#   resource_group_name = azurerm_resource_group.my_resource_group.name
+#   location            = azurerm_resource_group.my_resource_group.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
+#   tags = local.common_tags
+# }
 
-resource "azurerm_storage_container" "storage_container" {
-  name                  = "${local.resource_name_prefix}-container"
-  storage_account_id    = azurerm_storage_account.storage_account.id
-  container_access_type = "private"
-}
+# resource "azurerm_storage_container" "storage_container" {
+#   name                  = "${local.resource_name_prefix}-container"
+#   storage_account_id    = azurerm_storage_account.storage_account.id
+#   container_access_type = "private"
+# }
 
