5946945c40b3c3fbe6bf14cb9b97698ce50783d3
TemitopeAde22 <temitope224468@gmail.com>
2025-06-30 01:47:21 +0100

    replace bastion host with azure managed bastion service

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "my-resource-group-ukwest-dev"
-    storage_account_name = "ukwestdev-storageaccount"   # <-- match exactly
+    storage_account_name = "ukwestdevstorageaccount"   # <-- match exactly
     container_name       = "ukwestdev-container"        # <-- match exactly
     key                  = "terraform.tfstate"
   }
